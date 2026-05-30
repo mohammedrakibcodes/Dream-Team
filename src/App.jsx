bfbc526a6363b6c18017deaa8b7746ea6c942e36
Mohammed Rakib <mohammedrakibhossain02@gmail.com>
2026-05-30 18:27:53 +0600

    fix players json path

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import "./App.css";
 
+import { Suspense } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import AvailablePlayers from "./components/AvailablePlayers/AvailablePlayers";
-import { Suspense } from "react";
 // import SelectedPlayers from "./components/SelectedPlayers/SelectedPlayers";
 // import Newsletter from "./components/Newsletter/Newsletter";
 // import Footer from "./components/Footer/Footer";
 
 const fetchPlayer = async () => {
-  const res = await fetch("/Data/players.json");
+  const res = await fetch("Data/players.json");
   return res.json();
 };
 
 function App() {
   const playersPromise = fetchPlayer();
+
   return (
     <>
       <Navbar />
       <Hero />
+
       <Suspense
         fallback={
           <div className="flex w-52 flex-col gap-4">
@@ -29,7 +31,6 @@ function App() {
           </div>
         }
       >
-        {" "}
         <AvailablePlayers playersPromise={playersPromise} />
       </Suspense>
 
